0d13b53206ac8b63388508186afe41abb9e8650a
EC2 Default User <giran904587034+git@gmail.com>
2021-10-21 06:04:17 +0000

    java変数

---
+++ MyA.java
@@ -0,0 +1,11 @@
+public class MyA {
+  
+  public static void main(String[] args) {
+    
+    // String msg;
+    // msg = "Hello World Again!";
+    String msg = "Hello World Aaaa";
+    
+    System.out.println(msg);
+  }
+}
