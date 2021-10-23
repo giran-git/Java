f69a705fb93bb02c0e06d2d8d53fc5a1f0635663
EC2 Default User <giran904587034+git@gmail.com>
2021-10-23 09:12:09 +0000

    ドットインストール

---
+++ MyC.java
@@ -0,0 +1,27 @@
+public class MyC {
+
+
+  public static void main(String[] args) {
+    
+    // int i;
+    // i = 10 / 3;
+    // System.out.println(i);
+    // i = 10 % 3;
+    // System.out.println(i);
+    // int x = 5;
+    // x++;
+    // System.out.println(x);
+    // x--;
+    // System.out.println(x);
+    
+    // int x = 5;
+    // // x = x + 12;
+    // x += 12;
+    // System.out.println(x);
+    
+    String s;
+    s = "hello" + "world";
+    System.out.println(s);
+  }
+
+}
