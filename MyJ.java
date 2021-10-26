a6a05d435c1cda8ed4348b1c54019218f9d18d37
EC2 Default User <giran904587034+git@gmail.com>
2021-10-26 13:18:52 +0000

    プロゲート4-4インスタンスメゾット

---
+++ MyJ.java
@@ -0,0 +1,12 @@
+class MyJ {
+  public static void main(String[] args) {
+    Pj pj1 = new Pj();
+    // person1に対して、helloメソッドを呼び出してください
+    pj1.hello();
+    
+    Pj pj2 = new Pj();
+    // person2に対して、helloメソッドを呼び出してください
+    pj2.hello();
+    
+  }
+}
---
+++ Pj.java
@@ -0,0 +1,7 @@
+class Pj {
+  // インスタンスメソッドhelloを定義してください
+  public void hello() {
+    System.out.println("こんにちは");
+  }
+  
+}
