f5ba00ade2f39e44c8b8e5a509f4fc9a63ba4894
EC2 Default User <giran904587034+git@gmail.com>
2021-10-27 12:43:09 +0000

    4-8コンストラスタ

---
+++ pro-4/MyB.java
@@ -0,0 +1,11 @@
+class MyB {
+  public static void main(String[] args) {
+    // newの引数に「Kate Jones」を渡してください
+    Pb pb1 = new Pb("Kate Jones");
+    pb1.hello();
+
+    // newの引数に「John Christopher Smith」を渡してください
+    Pb pb2 = new Pb("John Christopher Smith");
+    pb2.hello();
+  }
+}
