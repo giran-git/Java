de03327548033b404f6dcc4e1768ecae76c9795f
EC2 Default User <giran904587034+git@gmail.com>
2021-10-27 12:44:30 +0000

    4-10フィールドとコンストラクタ

---
+++ pro-4/MyC.java
@@ -0,0 +1,11 @@
+class MyC {
+  public static void main(String[] args) {
+    Pc pc1 = new Pc("Kate", "Jones", 27, 1.6, 50.0);
+    
+    System.out.println(pc1.firstName);
+    System.out.println(pc1.lastName);
+    System.out.println(pc1.age);
+    System.out.println(pc1.height);
+    System.out.println(pc1.weight);
+  }
+}
