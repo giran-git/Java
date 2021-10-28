a01be79c2c3c82c2a80947d5c99fa49f5fe3085c
EC2 Default User <giran904587034+git@gmail.com>
2021-10-28 07:41:14 +0000

    4-11インスタンスメゾット

---
+++ pro-4/MyD.java
@@ -0,0 +1,11 @@
+class MyD {
+  public static void main(String[] args) {
+    Pd pc1 = new Pd("Kate", "Jones", 27, 1.6, 50.0);
+    
+    System.out.println(pc1.firstName);
+    System.out.println(pc1.lastName);
+    System.out.println(pc1.age);
+    System.out.println(pc1.height);
+    System.out.println(pc1.weight);
+  }
+}
